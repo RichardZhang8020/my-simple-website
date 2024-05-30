a9bd2498fd8ef180812164e3da4392d03367a0e3
Richard Zhang <richardzhang085@gmail.com>
2024-05-30 07:53:22 -0500

    add back in fix for blank screen

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import AboutPage from './AboutPage';
 
 function App() {
   return (
-    <Router>  
+    <Router basename={process.env.PUBLIC_URL}>  
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
@@ -13,7 +13,5 @@ function App() {
     </Router>
   );
 }
-
-// basename={process.env.PUBLIC_URL} HUH?
 
 export default App;
